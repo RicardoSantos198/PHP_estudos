d8be282f47a8ace43348ed6f2ab4463dba6c237b
Ricardo Santos <ricardosans351@gmail.com>
2026-09-05 18:21:59 -0300

    Estudos de PHP

---
+++ openmusic.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Bem-vindo(a) ao OpenMusic!\n";
+
+$nomeAlbum = "Love Gun";
+$anoLancamento = 1977;
+$artistaBanda = "Kiss";
+$preco = 175.00;
+
+$somaDeNotas = 9.5 + 8.1 + 3.6 + 10.0 + 8.9;
+$mediaAlbum = $somaDeNotas / 5;
+
+
+echo "Álbum: " . $nomeAlbum . "\n";
+echo "Média das notas: " . $mediaAlbum . "\n";
+// git remote add origin https://github.com/RicardoSantos198/PHP_estudos.git
+//git branch -M main
+//git push -u origin main
+//https://github.com/RicardoSantos198/PHP_estudos.git
