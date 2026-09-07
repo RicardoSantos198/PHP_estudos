4c863839fd700a8361b51ec2cc852cdca578bae1
Ricardo Santos <ricardosans351@gmail.com>
2026-09-07 18:34:58 -0300

    Praticando conceitos de for

--- openmusic.php
+++ openmusic.php
@@ -3,17 +3,45 @@
 echo "Bem-vindo(a) ao OpenMusic!\n";
 
 $nomeAlbum = "Love Gun";
-$anoLancamento = 1977;
+$anoLancamento =  1977;
 $artistaBanda = "Kiss";
 $preco = 175.00;
 
-$somaDeNotas = 9.5 + 8.1 + 3.6 + 10.0 + 8.9;
-$mediaAlbum = $somaDeNotas / 5;
+$quantidadedeNotas = $argc - 1;
+
+$somaDeNotas = 0;
+
+for ($contador = 1; $contador < $argc; $contador += 1) {
+    $somaDeNotas += $argv [$contador];
+}
+
+$mediaAlbum = $somaDeNotas / $quantidadedeNotas ;
 
 
 echo "Álbum: " . $nomeAlbum . "\n";
+echo "Ano de lançamento: "  . $anoLancamento ."\n";
 echo "Média das notas: " . $mediaAlbum . "\n";
-// git remote add origin https://github.com/RicardoSantos198/PHP_estudos.git
-//git branch -M main
-//git push -u origin main
-//https://github.com/RicardoSantos198/PHP_estudos.git
+
+if ($anoLancamento > 2022){
+    echo "Este album é lançamento.\n";
+}
+
+elseif ($anoLancamento > 2000 && $anoLancamento <= 2022){
+    echo"Este album não é tão antigo\n";
+
+}
+
+else {
+    echo "Este album não é lançamento\n";
+}
+
+$genero = match ($nomeAlbum) {
+    "Love Gun " => "Hard Rock",
+    "Vol.4" => "Metal",
+    "Nantucket Sleghride" => "Blues rock",
+    default => "Gênero desconhecido...",
+};
+
+echo "O genêro do album é: " . $nomeAlbum . "\n" ;
+
+echo $argc;
